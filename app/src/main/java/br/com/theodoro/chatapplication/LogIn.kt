5e7e779a60b15ce6e020227a6bcb8c149fb1755b
theomeme <theomeme21@gmail.com>
2023-04-16 17:08:43 -0300

    Signup Xml finished

--- app/src/main/java/br/com/theodoro/chatapplication/LogIn.kt
+++ app/src/main/java/br/com/theodoro/chatapplication/LogIn.kt
@@ -1,11 +1,24 @@
 package br.com.theodoro.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import br.com.theodoro.chatapplication.databinding.ActivityMainBinding
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var btnSignUp: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_log_in)
+
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
