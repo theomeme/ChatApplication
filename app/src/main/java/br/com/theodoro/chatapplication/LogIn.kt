224b48fca3a03b3f1d191263a6043ce42d30c8e1
theomeme <theomeme21@gmail.com>
2023-04-15 22:32:52 -0300

    Initial commit

---
+++ app/src/main/java/br/com/theodoro/chatapplication/LogIn.kt
@@ -0,0 +1,11 @@
+package br.com.theodoro.chatapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
