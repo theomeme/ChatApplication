ad11d3c5c5746ba3f38f31f3fdf4c8a1ec97864e
theomeme <theomeme21@gmail.com>
2023-04-19 16:46:33 -0300

    Firebase Auth Working

--- app/src/main/java/br/com/theodoro/chatapplication/LogIn.kt
+++ app/src/main/java/br/com/theodoro/chatapplication/LogIn.kt
@@ -4,21 +4,58 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import br.com.theodoro.chatapplication.databinding.ActivityMainBinding
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LogIn : AppCompatActivity() {
 
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_log_in)
 
+        auth = Firebase.auth
+
         btnSignUp = findViewById(R.id.btnSignUp)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
 
         btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            login(email, password)
+            }
+
+        }
+
+    private fun login(email: String, password: String) {
+        // logic for logging the user
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for logging in user
+                    val intent = Intent(this@LogIn, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@LogIn, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
--- app/src/main/java/br/com/theodoro/chatapplication/SignUp.kt
+++ app/src/main/java/br/com/theodoro/chatapplication/SignUp.kt
@@ -4,19 +4,56 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUp : AppCompatActivity() {
 
-    lateinit var btnPrevious: Button
+    private lateinit var btnPrevious: Button
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnCreate: Button
+    private lateinit var etName: EditText
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        auth = Firebase.auth
 
         btnPrevious = findViewById(R.id.btnPrevious)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnCreate = findViewById(R.id.btnCreate)
+        etName = findViewById(R.id.etName)
 
         btnPrevious.setOnClickListener {
             intent = Intent(this, LogIn::class.java)
             startActivity(intent)
         }
+
+        btnCreate.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            create(email, password)
+        }
+    }
+
+    private fun create(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // jumping for to home
+                    val intent = Intent(this@SignUp, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@SignUp, "Some error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
 }
