920d0e4fb4499a6df211e928c91a9e16d56f0133
theomeme <theomeme21@gmail.com>
2023-04-19 08:37:30 -0300

    Preparation for Firebase Auth

--- app/src/main/java/br/com/theodoro/chatapplication/SignUp.kt
+++ app/src/main/java/br/com/theodoro/chatapplication/SignUp.kt
@@ -1,11 +1,22 @@
 package br.com.theodoro.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var btnPrevious: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btnPrevious = findViewById(R.id.btnPrevious)
+
+        btnPrevious.setOnClickListener {
+            intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
     }
 }
